db52c066646a1041648019d8f42d864807adddf1
kyoheiAbc <autumn8november@gmail.com>
2023-02-18 08:01:07 +0900

    mob model

--- src/main/java/net/mcreator/ninjago/client/renderer/GarmadonRenderer.java
+++ src/main/java/net/mcreator/ninjago/client/renderer/GarmadonRenderer.java
@@ -7,12 +7,12 @@ import net.minecraft.client.renderer.entity.HumanoidMobRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.model.HumanoidModel;
-
+import net.mcreator.ninjago.client.model.GarmadonModel;
 import net.mcreator.ninjago.entity.GarmadonEntity;
 
 public class GarmadonRenderer extends HumanoidMobRenderer<GarmadonEntity, HumanoidModel<GarmadonEntity>> {
 	public GarmadonRenderer(EntityRendererProvider.Context context) {
-		super(context, new HumanoidModel(context.bakeLayer(ModelLayers.PLAYER)), 0.5f);
+		super(context, new GarmadonModel(GarmadonModel.LayerDefinitionCreate().bakeRoot()), 0.5f);
 		this.addLayer(new HumanoidArmorLayer(this, new HumanoidModel(context.bakeLayer(ModelLayers.PLAYER_INNER_ARMOR)),
 				new HumanoidModel(context.bakeLayer(ModelLayers.PLAYER_OUTER_ARMOR))));
 	}
