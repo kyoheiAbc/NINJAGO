ed07330b8f0a142a315dd809f4d19cb4e3bdfb30
kyoheiAbc <autumn8november@gmail.com>
2023-02-25 17:14:08 +0900

    reset

--- src/main/java/net/mcreator/ninjago/NinjagoMod.java
+++ src/main/java/net/mcreator/ninjago/NinjagoMod.java
@@ -29,9 +29,6 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.FriendlyByteBuf;
 
-import net.mcreator.ninjago.init.NinjagoModItems;
-import net.mcreator.ninjago.init.NinjagoModEntities;
-
 import java.util.function.Supplier;
 import java.util.function.Function;
 import java.util.function.BiConsumer;
@@ -48,9 +45,6 @@ public class NinjagoMod {
 		MinecraftForge.EVENT_BUS.register(this);
 
 		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-
-		NinjagoModItems.REGISTRY.register(bus);
-		NinjagoModEntities.REGISTRY.register(bus);
 
 	}
 
