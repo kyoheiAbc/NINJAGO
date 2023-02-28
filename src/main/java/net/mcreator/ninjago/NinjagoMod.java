8dcb936aa93cfa0ba23e1d0edd25a8676e47e993
kyoheiAbc <autumn8november@gmail.com>
2023-02-28 18:34:56 +0900

    mob

--- src/main/java/net/mcreator/ninjago/NinjagoMod.java
+++ src/main/java/net/mcreator/ninjago/NinjagoMod.java
@@ -29,6 +29,9 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.FriendlyByteBuf;
 
+import net.mcreator.ninjago.init.NinjagoModItems;
+import net.mcreator.ninjago.init.NinjagoModEntities;
+
 import java.util.function.Supplier;
 import java.util.function.Function;
 import java.util.function.BiConsumer;
@@ -45,6 +48,9 @@ public class NinjagoMod {
 		MinecraftForge.EVENT_BUS.register(this);
 
 		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+		NinjagoModItems.REGISTRY.register(bus);
+		NinjagoModEntities.REGISTRY.register(bus);
 
 	}
 
